3c97b54224c191c4319fd4d080fad24545fc588a
somnath <somnathmishra100dbi@gmail.com>
2025-06-27 01:36:00 +0530

    e2b_sandbox_setup

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { inngest } from "./client";
 import { gemini, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-shanu-121");
+      return sandbox.sandboxId;
+    });
+
     const summarizer = createAgent({
       name: "summarizer",
       system: "You summarize content in 2 words.",
@@ -14,6 +21,12 @@ export const helloWorld = inngest.createFunction(
     const { output } = await summarizer.run(
       `summarize the following text: ${event.data.text}`
     );
-    return output;
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+    return { output, sandboxUrl };
   }
 );
