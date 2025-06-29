fde5acac2a7a4fbdc575efa1abd2061da1a8fc2a
somnath <somnathmishra100dbi@gmail.com>
2025-06-29 16:41:15 +0530

    added_message_ui

---
+++ src/modules/projects/ui/views/project-view.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Suspense } from "react";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
+import { MessagesContainer } from "../components/messages-container";
+
+interface Props {
+  projectId: string;
+}
+
+export const ProjectView = ({ projectId }: Props) => {
+ 
+
+  // const { data: project } = useSuspenseQuery(
+  //   trpc.projects.getOne.queryOptions({
+  //     id: projectId,
+  //   })
+  // );
+
+  return (
+    <div className="h-screen">
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel
+          defaultSize={35}
+          minSize={20}
+          className="flex flex-col min-h-0"
+        >
+          <Suspense fallback={<p>Loading messages...</p>}>
+            <MessagesContainer projectId={projectId} />
+          </Suspense>
+        </ResizablePanel>
+
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={65} minSize={50}>
+          TODO: Preview
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
+};
+
+/**
+ * useSuspenseQuery is used (instead of regular useQuery) because it's expected   that data is already prefetched and hydrated from server.
+ */
